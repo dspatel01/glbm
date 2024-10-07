01d4ce7f8cc3a348fa32100f1f2233b28db047d4
dspatel01 <pateldileep290492@gmail.com>
2024-10-07 21:05:31 +0530

    card responsiveness & mobile navbar open width

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -38,23 +38,23 @@ const Navbar = () => {
         <div className='xl:hidden md:block sm:block' onClick={handleMenu} ><FaBars className='text-black text-[2.2rem]' /></div>
 
       </div>
-      <ul className={openMenu ? 'h-[100vh] w-[60%] bg-[rgba(255,255,255,0.8)] py-5 ml-0 transition-all xl:hidden lg:hidden md:block sm:block block' : 'transition-all h-[80vh] w-[60%] bg-[rgba(255,255,255,0.8)] py-5 ml-[-70%]'}>
-        <li className='ml-20 mt-5' onClick={() => setOpenMenu(false)}>
+      <ul className={openMenu ? 'h-[100vh] xl:w-[60%] md:w-[80%] sm:w-[100%] w-[100%] bg-[rgba(255,255,255,0.9)] py-5 ml-0 transition-all xl:hidden lg:hidden md:block sm:block block' : 'transition-all h-[80vh] w-[60%] bg-[rgba(255,255,255,0.8)] py-5 ml-[-70%]'}>
+        <li className='text-center mt-5' onClick={() => setOpenMenu(false)}>
           <NavLink className='text-[22px] text-gray-700 px-2 py-1 hover:border-b-2 font-bold hover:border-b-red-900  hover:text-red-900 transition-all' to='/' >
             Home
           </NavLink>
         </li>
-        <li className='ml-20 mt-5' onClick={() => setOpenMenu(false)}>
+        <li className='text-center mt-5' onClick={() => setOpenMenu(false)}>
           <NavLink className='text-[22px] text-gray-700 px-2 py-1 hover:border-b-2 font-bold hover:border-b-red-900  hover:text-red-900 transition-all' to='/aboutUs'>
             About
           </NavLink>
         </li>
-        <li className='ml-20 mt-5' onClick={() => setOpenMenu(false)}>
+        <li className='text-center mt-5' onClick={() => setOpenMenu(false)}>
           <NavLink className='text-[22px] text-gray-700 px-2 py-1 hover:border-b-2 font-bold hover:border-b-red-900  hover:text-red-900 transition-all' to='/gallery'>
             Service
           </NavLink>
         </li>
-        <li className='ml-20 mt-5' onClick={() => setOpenMenu(false)}>
+        <li className='text-center mt-5' onClick={() => setOpenMenu(false)}>
           <NavLink className='text-[22px] text-gray-700 px-2 py-1 hover:border-b-2 font-bold hover:border-b-red-900  hover:text-red-900 transition-all' to='/contactUs'>
             Contact
           </NavLink>
