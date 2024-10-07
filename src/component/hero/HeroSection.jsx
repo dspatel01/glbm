da0d21a6002b10921e80ad48044daa41fe2230fc
dspatel01 <pateldileep290492@gmail.com>
2024-10-07 20:58:56 +0530

    product responsive card & Gender mobile responsive currection

--- src/component/hero/HeroSection.jsx
+++ src/component/hero/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
         <div className='w-full h-[80vh] backgroundImage mt-[-70px]'>
             <div className='w-full h-[100%] flex justify-end items-center flex-col overlay '>
                 
-                <h1 className='text-[7vmax] font-bold text-white text-justify textShadow mb-0'>Welcome to <span className='text-red-900'>GLBM</span></h1>
+                <h1 className='text-[7vmax] font-bold text-white text-center textShadow mb-0'>Welcome to <span className='text-red-900'>GLBM</span></h1>
                 <p className='text-gray-100 xl:text-[1.4rem] md:text-[1.3rem] sm:text-[1.5rem] mb-20 text-[1.5rem] w-[80%] text-center capitalize italic'>we are the largest Garments menufaturer in delhi and working in india. </p>
             </div>
         </div>
